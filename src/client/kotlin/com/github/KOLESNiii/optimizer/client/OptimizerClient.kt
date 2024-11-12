3f742cab3c3cf85c0ec2819e965a2b57a0a21c90
KOLESNiii <timkolesnichenko05@gmail.com>
2024-11-12 21:32:13 +0000

    Add project files

---
+++ src/client/kotlin/com/github/KOLESNiii/optimizer/client/OptimizerClient.kt
@@ -0,0 +1,9 @@
+package com.github.KOLESNiii.optimizer.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class OptimizerClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/com/github/KOLESNiii/optimizer/Optimizer.kt
@@ -0,0 +1,9 @@
+package com.github.KOLESNiii.optimizer
+
+import net.fabricmc.api.ModInitializer
+
+class Optimizer : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
